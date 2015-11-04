4dbe07062b1f6de6427c99bcf2702575862b6ea4
Neerad Kumar <neerad.kumar@gmail.com>
2015-11-04 20:06:20 +0600

    Added onclick listeners

--- app/src/main/java/org/nsu/myapplication/MainActivity.java
+++ app/src/main/java/org/nsu/myapplication/MainActivity.java
@@ -2,15 +2,18 @@ package org.nsu.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    //https://github.com/neeradkumar/CustomArrayAdapter
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         PersonItem atiar = new PersonItem("Atiar","Swajan",R.drawable.panda);
         PersonItem jibne = new PersonItem("Kamal","Jibne",R.drawable.monkey);
         PersonItem nafis = new PersonItem("Nafis","UI",R.drawable.cat);
-        ArrayList<PersonItem> personItems = new ArrayList<>();
+        final ArrayList<PersonItem> personItems = new ArrayList<>();
         personItems.add(atiar);
         personItems.add(jibne);
         personItems.add(nafis);
@@ -30,5 +33,17 @@ public class MainActivity extends AppCompatActivity {
                 new PersonAdapter(this, personItems);
 
         listView.setAdapter(nameAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view, int i, long id) {
+                PersonItem personItem = personItems.get(i);
+                String name = personItem.name;
+                Toast.makeText(MainActivity.this,
+                        "I clicked name "+name,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
